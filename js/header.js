2cbbcdf627fc58ca1b827a552e8034b81535d77f
Dima34 <vavilov9823@gmail.com>
2022-02-20 12:53:36 +0200

    search added

--- js/header.js
+++ js/header.js
@@ -1,14 +1,49 @@
-const headerMiddle = document.querySelector(".header_middle");
 const searchOpen = document.getElementById("search-open");
 const searchClose = document.getElementById("header_search-bar-close");
-
-console.log(searchOpen);
-console.log(searchClose);
+const searchBar = document.getElementById("search-bar");
+const searchResults = document.getElementById("search-block")
+const blur = document.getElementById("blur");
+const header = document.querySelector("header");
+const body = document.querySelector("body");
 
 searchOpen.addEventListener("click", ()=>{
-    headerMiddle.classList.add("search--opened")
+    openSearch()
 })
 
 searchClose.addEventListener("click", ()=>{
-    headerMiddle.classList.remove("search--opened")
+    closeSearch()
 })
+
+searchBar.addEventListener("input", ()=>{
+    searchBar.value != "" ? searchResults.classList.add("opened") : searchResults.classList.remove("opened")
+})
+
+function openSearch() {
+    header.classList.add("search--opened")
+    showBlur()
+    blockScroll();
+}
+
+function closeSearch() {
+    header.classList.remove("search--opened")
+    searchResults.classList.remove("opened")
+    searchBar.value = ""
+    hideBlur()
+    unblockScroll()
+}
+
+function showBlur() {
+    blur.classList.add("active")
+}
+
+function hideBlur() {
+    blur.classList.remove("active")
+}
+
+function blockScroll() {
+    body.classList.add("scroll-block")
+}
+
+function unblockScroll() {
+    body.classList.remove("scroll-block")
+}
