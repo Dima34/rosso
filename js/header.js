9a4b03a5786c1af9d4cb7ed81e641709d118f574
Dima34 <vavilov9823@gmail.com>
2022-02-22 12:24:58 +0200

    popups system improved

--- js/header.js
+++ js/header.js
@@ -62,77 +62,33 @@ function toogleMenu(trigger,menuElement, openFunc=()=>{}){
     }
 }
 
+popupTriggers.forEach(el=>{
+    el.addEventListener("click", ()=>{
+        if(el.dataset.customopenfunction){
+            //  https://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string#:~:text=You%20just%20need%20convert%20your,use%20it%20like%20a%20pointer.
+            toogleMenu(el,document.querySelector(el.dataset.popuptrigger), window[el.dataset.customopenfunction])
+        } else{
+            toogleMenu(el,document.querySelector(el.dataset.popuptrigger))
+        }
+        
+    })
+})
+
 // Search
-const searchOpen = document.getElementById("search-open");
-const searchClose = document.getElementById("header_search-bar-close");
-const searchBlock = document.querySelector(".header_middle")
 const searchBar = document.getElementById("search-bar");
 const searchResults = document.getElementById("search-block")
-
-searchOpen.addEventListener("click", ()=>{
-    toogleMenu(searchOpen,searchBlock)
-})
-
-searchClose.addEventListener("click", ()=>{
-    closeAll()
-})
 
 searchBar.addEventListener("input", ()=>{
     searchBar.value != "" ? searchResults.classList.add("opened") : searchResults.classList.remove("opened")
 })
 
-
-// Authorization
-const authOpenButton = document.querySelectorAll(".authorization-open")
-const authBlock = document.getElementById("authorization")
-const authCloseButton = document.getElementById("authorization-close")
-
-authOpenButton.forEach(el=>{
-    el.addEventListener("click", ()=>{
-        toogleMenu(el,authBlock)
-    })
-})
-
-authCloseButton.addEventListener("click", ()=>{
-    closeAll()
-})
-
-// Cart
-const cartOpenButton = document.getElementById("cart-open")
-const cartBlock = document.getElementById("cart")
-const cartCloseButton = document.getElementById("cart-close")
-
-cartOpenButton.addEventListener("click", ()=>{
-    toogleMenu(cartOpenButton,cartBlock)
-})
-
-cartCloseButton.addEventListener("click", ()=>{
-    closeAll()
-})
-
-// Burger
-const burgerOpenButton = document.getElementById("burger-open")
-const burgerBlock = document.getElementById("burger")
-
-burgerOpenButton.addEventListener("click", ()=>{
-    toogleMenu(burgerOpenButton,burgerBlock)
-})
-
 // Catalogue
-const catalogueOpenButton = document.querySelectorAll(".catalogue-open")
-const catalogueBlock = document.getElementById("catalogue")
-
 // Beacause catalugue pop up hasn`t his own close btn, so we must simulate it by 
 // Setting active burger btn to active (if we will clicl on it then all popups will close with catalogue)
 function createCatalogueClosebtn(){
-    burgerOpenButton.classList.add("active")
+    let burgerBtn = document.getElementById("burger-open")
+    burgerBtn.classList.add("active")
 }
-
-catalogueOpenButton.forEach(el=>{
-    el.addEventListener("click", ()=>{
-        toogleMenu(el,catalogueBlock,createCatalogueClosebtn)
-    })
-})
 
 const unfoldButton = document.querySelectorAll(".catalogue-item__unfold");
 
