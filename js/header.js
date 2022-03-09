8c0b8a6a2639fd5af1c4db2a725d9b22e9cf2fad
Dima Laptop <vavilov9823@gmail.com>
2022-03-09 18:15:27 +0200

    some spaces removed

--- js/header.js
+++ js/header.js
@@ -6,79 +6,79 @@ const popupTriggers = document.querySelectorAll("[data-popupTrigger]")
 const closeTriggers = document.querySelectorAll("[data-popupCloseTrigger]")
 
 function showBlur() {
-    blur.classList.add("active")
+  blur.classList.add("active")
 }
 
 function hideBlur() {
-    blur.classList.remove("active")
+  blur.classList.remove("active")
 }
 
 function blockScroll() {
-    body.classList.add("scroll-block")
+  body.classList.add("scroll-block")
 }
 
 function unblockScroll() {
-    body.classList.remove("scroll-block")
+  body.classList.remove("scroll-block")
 }
 
 function makeHeaderWhite(){
-    header.classList.add("white-bg");
+  header.classList.add("white-bg");
 }
 
 function removeWhiteHeader(){
-    header.classList.remove("white-bg")
+  header.classList.remove("white-bg")
 }
 
 function closeAll(){
-    popups.forEach(el=>{
-        el.classList.remove("active")
-    })
-    popupTriggers.forEach(el=>{
-        el.classList.remove("active")
-    })
-    hideBlur()
-    unblockScroll()
-    removeWhiteHeader()
+  popups.forEach(el=>{
+    el.classList.remove("active")
+  })
+  popupTriggers.forEach(el=>{
+    el.classList.remove("active")
+  })
+  hideBlur()
+  unblockScroll()
+  removeWhiteHeader()
 }
 
 function toogleMenu(trigger,menuElement, openFunc=()=>{}){
-    // That statement allows us to close a mobile catalogue without opening burger(because them are on one button)
-    // *if catalogue opened burger button are active and burger arent, so we need to click burger button and don`t 
-    //  open the burger, kust close the catalogue, so we check if the button has cative and if it has we close all
-    //  popups and dont open the related to button popup
-    if(menuElement.classList.contains("active") || trigger.classList.contains("active") ){
-        closeAll()
-        trigger.classList.remove("active")
-        menuElement.classList.remove("active")
-        hideBlur()
-        unblockScroll();
-    } else{
-        closeAll()
-        openFunc()
-        trigger.classList.add("active")
-        menuElement.classList.add("active")
-        showBlur()
-        blockScroll();
-        makeHeaderWhite()
-    }
+  // That statement allows us to close a mobile catalogue without opening burger(because them are on one button)
+  // *if catalogue opened burger button are active and burger arent, so we need to click burger button and don`t 
+  //  open the burger, kust close the catalogue, so we check if the button has cative and if it has we close all
+  //  popups and dont open the related to button popup
+  if(menuElement.classList.contains("active") || trigger.classList.contains("active") ){
+    closeAll()
+    trigger.classList.remove("active")
+    menuElement.classList.remove("active")
+    hideBlur()
+    unblockScroll();
+  } else{
+    closeAll()
+    openFunc()
+    trigger.classList.add("active")
+    menuElement.classList.add("active")
+    showBlur()
+    blockScroll();
+    makeHeaderWhite()
+  }
 }
 
 closeTriggers.forEach(el=>{
-    el.addEventListener("click", ()=>{
-        closeAll()
-    })
+  el.addEventListener("click", ()=>{
+    closeAll()
+  })
 })
 
 popupTriggers.forEach(el=>{
-    el.addEventListener("click", ()=>{
-        if(el.dataset.customopenfunction){
-            //  https://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string#:~:text=You%20just%20need%20convert%20your,use%20it%20like%20a%20pointer.
-            toogleMenu(el,document.querySelector(el.dataset.popuptrigger), window[el.dataset.customopenfunction])
-        } else{
-            toogleMenu(el,document.querySelector(el.dataset.popuptrigger))
-        }
-        
-    })
+  el.addEventListener("click", ()=>{
+    if(el.dataset.customopenfunction){
+      //  https://stackoverflow.com/questions/359788/how-to-execute-a-javascript-function-when-i-have-its-name-as-a-string#:~:text=You%20just%20need%20convert%20your,use%20it%20like%20a%20pointer.
+      toogleMenu(el,document.querySelector(el.dataset.popuptrigger), window[el.dataset.customopenfunction])
+    } else{
+      toogleMenu(el,document.querySelector(el.dataset.popuptrigger))
+    }
+    
+  })
 })
 
 // Search
@@ -86,7 +86,7 @@ const searchBar = document.getElementById("search-bar");
 const searchResults = document.getElementById("search-block")
 
 searchBar.addEventListener("input", ()=>{
-    searchBar.value != "" ? searchResults.classList.add("opened") : searchResults.classList.remove("opened")
+  searchBar.value != "" ? searchResults.classList.add("opened") : searchResults.classList.remove("opened")
 })
 
 // Catalogue
@@ -108,7 +108,7 @@ unfoldButton.forEach(el=>{
 					el.style.display = "none"
 			}
 	})
-    
+  
 })
 
 function foldAll(){
